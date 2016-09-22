2384046b36b6b0434938e3b84b4124c93dfc8b94
MacBim <jean-baptiste.mercier@iut-valence.fr>
2016-09-22 16:11:05 +0200

    Add files via upload

--- TRUECeuilletteASCII/src/Map.java
+++ TRUECeuilletteASCII/src/Map.java
@@ -1,21 +1,22 @@
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class Map {
 	
 	String[][] content;
 	
-	public Map(int size){
-		this.content = new String[size][size];
-//		this.content[0][1] = "0";
-//		this.content[1][1] = "1";
-//		this.content[2][1] = "2";
-//		this.content[3][1] = "3";
-//		this.content[4][1] = "4";
-		for(int x=0; x<size;x++){
-			for(int y=0; y<size;y++){
-				this.content[x][y] = " @ ";
+	public Map(List mapContent){
+		for (int i = 0; i < mapContent.size(); i++) {
+			if(i==0){
+				int size = (int) mapContent.get(i);
+				this.content = new String[size][size];
+			} else {
+				Position tmp = (Position) mapContent.get(i);
+				this.content[tmp.getX()][tmp.getY()] = " * ";
 			}
 		}
-//		System.out.println(this.content.length);
 		/**
 		 * pour chaque agent, le placer dans le tableau content
 		 * pareil pour les patch
@@ -35,8 +36,13 @@ public class Map {
 		String asciiMap = "";
 		for(int x=0; x<this.content.length;x++){
 			for(int y=0; y<this.content.length;y++){
-				System.out.println(x+" | "+y+"\n");
-				asciiMap += this.content[x][y];
+//				System.out.println(x+" | "+y+"\n");
+				if(this.content[x][y]==null){
+					asciiMap += " ";
+				} else {
+					asciiMap += this.content[x][y];
+				}
+				
 			}
 			
 			asciiMap += '\n';
--- TRUECeuilletteASCII/src/main.java
+++ TRUECeuilletteASCII/src/main.java
@@ -1,11 +1,17 @@
+import java.util.List;
+import java.io.IOException;
+
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Parser parser = new Parser();
+
+		List list = (List) parser.parse("W:/git/TRUECeuilletteASCII/TRUECeuilletteASCII/src/map.txt");
+
 		// TODO Auto-generated method stub
-		Map map = new Map(10);
+		Map map = new Map(list);
 		System.out.println(map.drawMap());
-		System.out.println("OUI");
 	}
 
 }
